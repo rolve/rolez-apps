1b6164910535a0778267cac379ad54dfb23df19a
Michael Faes <rolve@trick17.ch>
2016-02-29 16:49:11 +0100

    Correct slicing and recurse condition in Quicksort
    * Test that checks sorting correctness

--- quicksort/src/test/java/ch/trick17/rolezapps/quicksort/QuicksortTest.java
+++ quicksort/src/test/java/ch/trick17/rolezapps/quicksort/QuicksortTest.java
@@ -4,12 +4,14 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import rolez.lang.GuardedArray;
+
 public class QuicksortTest {
-    
-    private final Quicksort quicksort = new Quicksort();
     
     @Test
     public void testLog2() {
+        Quicksort quicksort = new Quicksort();
+        
         assertEquals(0, quicksort.log2(1));
         assertEquals(1, quicksort.log2(2));
         assertEquals(2, quicksort.log2(4));
@@ -17,5 +19,17 @@ public class QuicksortTest {
         
         assertEquals(1, quicksort.log2(3));
         assertEquals(4, quicksort.log2(31));
+    }
+    
+    @Test
+    public void testSort() {
+        // Only tests the single-threaded sort implementation
+        Quicksort quicksort = new Quicksort(1);
+        
+        int n = 1000;
+        GuardedArray<int[]> ints = quicksort.shuffledInts(n);
+        quicksort.sort(ints);
+        for(int i = 0; i < n; i++)
+            assertEquals(i, ints.data[i]);
     }
 }
