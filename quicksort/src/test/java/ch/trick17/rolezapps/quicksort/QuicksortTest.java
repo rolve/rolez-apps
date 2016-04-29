76328d8b5221d6709f9944c8840ef1c6890f941c
Michael Faes <rolve@trick17.ch>
2016-04-29 13:37:49 +0200

    Removed obsolete code in QuicksortTest

--- quicksort/src/test/java/ch/trick17/rolezapps/quicksort/QuicksortTest.java
+++ quicksort/src/test/java/ch/trick17/rolezapps/quicksort/QuicksortTest.java
@@ -9,19 +9,6 @@ import rolez.lang.GuardedArray;
 public class QuicksortTest {
     
     @Test
-    public void testLog2() {
-        Quicksort quicksort = new Quicksort();
-        
-        assertEquals(0, quicksort.log2(1));
-        assertEquals(1, quicksort.log2(2));
-        assertEquals(2, quicksort.log2(4));
-        assertEquals(4, quicksort.log2(16));
-        
-        assertEquals(1, quicksort.log2(3));
-        assertEquals(4, quicksort.log2(31));
-    }
-    
-    @Test
     public void testSort() {
         // Only tests the single-threaded sort implementation
         Quicksort quicksort = new Quicksort(1);
