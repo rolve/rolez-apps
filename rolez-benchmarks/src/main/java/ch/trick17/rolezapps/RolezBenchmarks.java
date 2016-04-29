8e9f8e36bd040408bcb7c3c76eb93068e53686da
Michael Faes <rolve@trick17.ch>
2016-04-29 14:02:15 +0200

    Specify measurement interations to get more than one...

--- rolez-benchmarks/src/main/java/ch/trick17/rolezapps/RolezBenchmarks.java
+++ rolez-benchmarks/src/main/java/ch/trick17/rolezapps/RolezBenchmarks.java
@@ -8,7 +8,8 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 public class RolezBenchmarks {
     
     public static void main(String[] args) throws RunnerException {
-        Options options = new OptionsBuilder().include(".*").warmupIterations(20).build();
+        Options options = new OptionsBuilder().include(".*")
+                .warmupIterations(20).measurementIterations(30).build();
         new Runner(options).run();
     }
 }
