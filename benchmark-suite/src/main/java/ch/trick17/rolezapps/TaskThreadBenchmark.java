e4164fe6ef5a2160885f4ae77668820ceec2cbb6
Michael Faes <rolve@trick17.ch>
2016-06-13 17:08:36 +0200

    Removed task creation overhead from TaskThreadBenchmark and got some new results: currentTask is more than 2x slower...
    * Also added some other operations for reference

--- benchmark-suite/src/main/java/ch/trick17/rolezapps/TaskThreadBenchmark.java
+++ benchmark-suite/src/main/java/ch/trick17/rolezapps/TaskThreadBenchmark.java
@@ -1,39 +1,77 @@
 package ch.trick17.rolezapps;
 
+import static org.openjdk.jmh.annotations.Level.Trial;
 import static org.openjdk.jmh.annotations.Mode.Throughput;
+import static org.openjdk.jmh.annotations.Scope.Benchmark;
 
-import java.util.concurrent.Callable;
+import java.lang.reflect.Field;
+import java.util.Deque;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import rolez.lang.Task;
-import rolez.lang.TaskSystem;
 
 @BenchmarkMode(Throughput)
 @Fork(1)
+@Warmup(iterations = 10)
+@State(Benchmark)
 public class TaskThreadBenchmark {
+    
+    Object field;
+    volatile Object volatileField;
+    final ThreadLocal<Object> threadLocal = new ThreadLocal<>();
+    
+    @Setup(Trial)
+    public void pushTask() throws NoSuchFieldException, IllegalAccessException {
+        getStack().push(new Task<Object>(null));
+        threadLocal.set(new Object());
+    }
+    
+    @TearDown(Trial)
+    public void popTask() throws NoSuchFieldException, IllegalAccessException {
+        getStack().pop();
+        threadLocal.set(null);
+    }
+    
+    @SuppressWarnings("unchecked")
+    Deque<Task<?>> getStack() throws NoSuchFieldException, IllegalAccessException {
+        Field stackField = Task.class.getDeclaredField("localStack");
+        stackField.setAccessible(true);
+        return ((ThreadLocal<Deque<Task<?>>>) stackField.get(null)).get();
+    }
     
     @Benchmark
     public Task<?> currentTask() {
-        return TaskSystem.getDefault().run(new Callable<Task<?>>() {
-            public Task<?> call() throws Exception {
-                return Task.currentTask();
-            }
-        });
+        return Task.currentTask();
     }
     
     @Benchmark
     public Thread currentThread() {
-        return TaskSystem.getDefault().run(new Callable<Thread>() {
-            public Thread call() throws Exception {
-                return Thread.currentThread();
-            }
-        });
+        return Thread.currentThread();
+    }
+    
+    @Benchmark
+    public Object threadLocalAccess() {
+        return threadLocal.get();
+    }
+    
+    @Benchmark
+    public Object fieldRead() {
+        return field;
+    }
+    
+    @Benchmark
+    public Object volatileFieldRead() {
+        return volatileField;
     }
     
     public static void main(String[] args) throws RunnerException {
