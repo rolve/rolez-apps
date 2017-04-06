baa7a91e8ef0fcb3dc17425e1b350a0d98707962
Michael Faes <rolve@trick17.ch>
2017-04-06 13:01:24 +0200

    Speedup plots!

--- benchmark-utils/src/main/resources/ch/trick17/rolezapps/plot.R
+++ benchmark-utils/src/main/resources/ch/trick17/rolezapps/plot.R
@@ -1,12 +1,14 @@
 library(tidyverse)
 library(scales)
 
-data <- read_tsv("results.tsv")
+data <- read_tsv("results.tsv") %>%
+  mutate(
+    impl  = factor(impl, levels = unique(data$impl)),
+    tasks = factor(tasks)
+  )
 
-implLevels <- (data %>% select(impl) %>% distinct)$impl
-data <- mutate(data, impl = factor(impl, levels = implLevels))
-
-plot <- ggplot(data, aes(factor(tasks), time, fill = impl)) +
+# Execution time bar plots
+plot <- ggplot(data, aes(tasks, time, fill = impl)) +
   geom_bar(
     stat = "summary",
     width = 0.9,
@@ -17,12 +19,6 @@ plot <- ggplot(data, aes(factor(tasks), time, fill = impl)) +
     width = 0.2,
     position = position_dodge(width = 0.9)
   ) +
-  # geom_text(
-  #   data = data %>% filter(impl == "Rolez") %>% group_by(impl, n, tasks) %>% summarise(time = mean(time)),
-  #   aes(label = time),
-  #   check_overlap = TRUE,
-  #   vjust = "outward"
-  # ) +
   facet_wrap(
     ~n,
     scales = "free_y",
@@ -39,4 +35,38 @@ plot <- ggplot(data, aes(factor(tasks), time, fill = impl)) +
   )
 
 ns <- data %>% select(n) %>% distinct() %>% count %>% unlist(use.names = FALSE)
-ggsave("results.pdf", plot, width = 3 * ns, height = 4)
+ggsave("execution_time.pdf", plot, width = 3 * ns, height = 4)
+
+
+# Self-relative speedup plots
+mean_times <- data %>%
+  group_by(benchmark, impl, n, tasks) %>%
+  summarize(time = mean(time))
+
+base_times <- mean_times %>%
+  filter(tasks == 1) %>%
+  select(-tasks) %>%
+  rename(base_time = time)
+
+speedups <- mean_times %>%
+  left_join(base_times, suffix = c("", "_base")) %>%
+  mutate(speedup = base_time / time)
+
+plot <- ggplot(speedups, aes(tasks, speedup, color = impl)) +
+  geom_point() +
+  geom_line(aes(group = impl)) +
+  facet_wrap(
+    ~n,
+    labeller = as_labeller(function(labels) {
+      return(paste0("n = ", labels))
+    })) +
+  scale_x_discrete("Tasks") + 
+  scale_y_continuous("Speedup") +
+  scale_color_discrete(NULL) +
+  theme(
+    panel.grid.major.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    legend.position = "bottom"
+  )
+
+ggsave("speedup.pdf", plot, width = 3 * ns, height = 4)
