978ffedb4111bb025efddc72e34a9c6b11339584
Michael Faes <rolve@trick17.ch>
2017-04-09 21:14:20 +0200

    A LocalOpt version for histogram, but it doesn't change much about the performance...

--- histogram/src/main/java/ch/trick17/rolezapps/histogram/HistogramBenchmark.java
+++ histogram/src/main/java/ch/trick17/rolezapps/histogram/HistogramBenchmark.java
@@ -31,7 +31,7 @@ public class HistogramBenchmark {
     @Param({"6000000", "24000000", "50000000"})
     int n;
     
-    @Param({"Rolez", "RolezImmutable", "Java"})
+    @Param({"Rolez", "RolezLocalOpt", "RolezImmutable", "Java"})
     String impl;
     
     @Param({"1", "2", "4", "8", "16", "32"})
