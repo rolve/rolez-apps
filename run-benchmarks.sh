4b159ecfeafb5db0c94965c799d21b23007eb6a4
Michael Faes <rolve@trick17.ch>
2017-04-09 15:41:53 +0200

    script to run all benchmarks and collect results

---
+++ run-benchmarks.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+benchmarks="animator histogram idea k-means mergesort montecarlo quicksort raytracer"
+
+
+for b in $benchmarks
+do
+    cd $b
+    mvn clean compile exec:java
+    cp *.pdf ..
+    cd ..
+done
+
+
+read first _ <<< "$benchmarks"
+head -n 1 "$first/results.tsv" > all_results.tsv
+
+for b in $benchmarks
+do
+    tail -n +2 "$b/results.tsv" >> all_results.tsv
+done
