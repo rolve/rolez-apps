a48dadd9040271d49530b9eaf03f6860cdd8eb5c
Michael Faes <rolve@trick17.ch>
2017-04-11 13:52:02 +0200

    Enabled normal "Rolez" version of benchmarks again

--- mergesort/src/main/java/ch/trick17/rolezapps/mergesort/MergesortBenchmark.java
+++ mergesort/src/main/java/ch/trick17/rolezapps/mergesort/MergesortBenchmark.java
@@ -29,7 +29,7 @@ public class MergesortBenchmark {
     @Param({"1000000", "5000000", "20000000"})
     int n;
     
-    @Param({"RolezL", "Java"})
+    @Param({"Rolez", "RolezL", "Java"})
     String impl;
     
     @Param({"1", "2", "4", "8", "16", "32"})
