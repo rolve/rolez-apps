9c538f238895a7d64a37e59fc5c5648c59e35d4d
Michael Faes <rolve@trick17.ch>
2017-07-05 10:20:34 +0200

    A benchmark for array copy. Guarding is the killer.

---
+++ benchmark-suite/src/main/java/ch/trick17/rolezapps/ArrayCopyBenchmark.java
@@ -0,0 +1,101 @@
+package ch.trick17.rolezapps;
+
+import static org.openjdk.jmh.annotations.Level.Iteration;
+import static org.openjdk.jmh.annotations.Mode.SingleShotTime;
+import static org.openjdk.jmh.annotations.Scope.Thread;
+import static rolez.lang.Guarded.guardReadOnly;
+import static rolez.lang.Guarded.guardReadWrite;
+import static rolez.lang.Task.currentTask;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import rolez.lang.Guarded;
+import rolez.lang.GuardedArray;
+import rolez.lang.Task;
+
+@BenchmarkMode(SingleShotTime)
+@Fork(1)
+@State(Thread)
+@Warmup(iterations = 30)
+@Measurement(iterations = 30)
+public class ArrayCopyBenchmark {
+	
+	@Param({"1000000", "10000000", "100000000"})
+	int n;
+
+	byte[] src;
+	byte[] dst;
+	GuardedArray<byte[]> guardedSrc;
+	GuardedArray<byte[]> guardedDst;
+	
+	@Setup(Iteration)
+	public void setup() {
+        Task.registerNewRootTask();
+		
+		src = new byte[n];
+		dst = new byte[n];
+		guardedSrc = new GuardedArray<byte[]>(new byte[n]);
+		guardedDst = new GuardedArray<byte[]>(new byte[n]);
+		for(int i = 0; i < n; i++) {
+			src[i] = (byte) i;
+			guardedSrc.data[i] = (byte) i;
+		}
+	}
+	
+	@Benchmark
+	public void arrayCopy() {
+		System.arraycopy(src, 0, dst, 0, src.length);
+	}
+	
+	@Benchmark
+	public void forLoop() {
+		byte[] src = this.src;
+		byte[] dst = this.dst;
+		for(int i = 0; i < src.length; i++)
+			dst[i] = src[i];
+	}
+	
+	@Benchmark
+	public void forLoopSrcGuarded() {
+		long $task = currentTask().idBits();
+		int n = src.length;
+		
+		GuardedArray<byte[]> src = this.guardedSrc;
+		byte[] dst = this.dst;
+		for(int i = 0; i < n; i++)
+			dst[i] = guardReadOnly(src, $task).data[i];
+	}
+	
+	@Benchmark
+	public void forLoopBothGuarded() {
+		long $task = currentTask().idBits();
+		int n = src.length;
+		
+		GuardedArray<byte[]> src = this.guardedSrc;
+		GuardedArray<byte[]> dst = this.guardedDst;
+		for(int i = 0; i < n; i++)
+			guardReadWrite(dst, $task).data[i] = guardReadOnly(src, $task).data[i];
+	}
+	
+    @TearDown(Level.Iteration)
+    public void tearDown() {
+        Task.unregisterRootTask();
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        String name = ArrayCopyBenchmark.class.getSimpleName();
+        new Runner(new OptionsBuilder().include(name).build()).run();
+    }
+}
