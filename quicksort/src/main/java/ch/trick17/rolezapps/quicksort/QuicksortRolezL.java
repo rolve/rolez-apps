4c10901bad9d1468bc696bd9fe9905bb641e5404
Michael Faes <rolve@trick17.ch>
2017-08-21 10:56:03 +0200

    Removed unnecessary methods in quicksort benchmark (by now, Rolez versions are at least as efficient as the manually written ones)

--- quicksort/src/main/java/ch/trick17/rolezapps/quicksort/QuicksortRolezL.java
+++ quicksort/src/main/java/ch/trick17/rolezapps/quicksort/QuicksortRolezL.java
@@ -1,7 +1,6 @@
 package ch.trick17.rolezapps.quicksort;
 
 import rolez.lang.GuardedSlice;
-import rolez.lang.Task;
 import rolez.lang.TaskSystem;
 
 public class QuicksortRolezL extends QuicksortRolez {
@@ -45,75 +44,6 @@ public class QuicksortRolezL extends QuicksortRolez {
             
             if(sortRight)
                 this.doSort(s, left, end, level + 1, $task);
-        }
-    }
-    
-    @Override
-    public Task<Void> doSort$Task(final GuardedSlice<int[]> s, final int begin, final int end,
-            final int level) {
-        return new Task<Void>(new Object[]{s}, new Object[]{}) {
-            @Override
-            protected Void runRolez() {
-                long $task = idBits();
-                int pivot = pivot(s, begin, end, $task);
-                int left = begin;
-                int right = end - 1;
-                while(left <= right) {
-                    while(s.getInt(left) < pivot)
-                        left += 1;
-                    while(s.getInt(right) > pivot)
-                        right -= 1;
-                    if(left <= right) {
-                        int temp = s.getInt(left);
-                        s.setInt(left, s.getInt(right));
-                        s.setInt(right, temp);
-                        left += 1;
-                        right -= 1;
-                    }
-                }
-                boolean sortLeft = begin < right;
-                boolean sortRight = left < (end - 1);
-                if(level < maxLevel) {
-                    if(sortLeft)
-                        TaskSystem.getDefault().start(doSort$Task(s.slice(begin, right + 1, 1),
-                                begin, right + 1, level + 1));
-                    
-                    if(sortRight)
-                        doSort(s.slice(left, end, 1), left, end, level + 1, $task);
-                }
-                else {
-                    if(sortLeft)
-                        doSort(s, begin, right + 1, level + 1, $task);
-                    
-                    if(sortRight)
-                        doSort(s, left, end, level + 1, $task);
-                }
-                return null;
-            }
-        };
-    }
-    
-    @Override
-    public int pivot(final GuardedSlice<int[]> s, int begin, int end, long $task) {
-        guardReadOnly(s, $task);
-        int l = s.getInt(begin);
-        int m = s.getInt(begin + ((end - begin) / 2));
-        int r = s.getInt(end - 1);
-        if(l < m) {
-            if(m < r)
-                return m;
-            else if(l < r)
-                return l;
-            else
-                return r;
-        }
-        else {
-            if(l < r)
-                return l;
-            else if(m < r)
-                return r;
-            else
-                return m;
         }
     }
 }
