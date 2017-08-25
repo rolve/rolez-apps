0c3edb11eac8167ea14a92cf5e18838ec65ae953
Michael Faes <rolve@trick17.ch>
2017-08-25 14:58:15 +0200

    Slices versions again

--- animator/src/main/java/ch/trick17/rolezapps/raytracer/anim/AnimatorBenchmark.java
+++ animator/src/main/java/ch/trick17/rolezapps/raytracer/anim/AnimatorBenchmark.java
@@ -27,7 +27,7 @@ public class AnimatorBenchmark {
     @Param({"45", "90", "180"})
     int n; //height
     
-    @Param({"RolezNoSlices", "Java"})
+    @Param({"Rolez", "RolezNoSlices", "Java"})
     String impl;
     
     @Param({"1", "2", "4", "8", "16", "32"})
