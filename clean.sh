72d19191d7e41fae07b4c6291507a44e627d9228
micgiger <micgiger@student.ethz.ch>
2017-11-13 16:08:00 +0100

    fix parameterized script

--- clean.sh
+++ clean.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-rm -r $1/lib/idea
+rm -r $1/lib/$1
 rm $1/lib/checked.jar
 
 cd $1
 
-mvn dependency:purge-local-repository -DmanualInclude="idea:idea"
+mvn dependency:purge-local-repository -DmanualInclude="$1:$1"
