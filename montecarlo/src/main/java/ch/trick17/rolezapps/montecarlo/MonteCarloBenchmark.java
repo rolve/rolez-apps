72b45d81000f30499c3a97ee34fb22633fee6c8b
Michael Faes <rolve@trick17.ch>
2018-07-01 22:20:18 +0200

    Remove non-eager Monte Carlo implementation

--- montecarlo/src/main/java/ch/trick17/rolezapps/montecarlo/MonteCarloBenchmark.java
+++ montecarlo/src/main/java/ch/trick17/rolezapps/montecarlo/MonteCarloBenchmark.java
@@ -45,7 +45,7 @@ public class MonteCarloBenchmark {
     @Param({"1", "2", "4", "8", "16", "32"})
     int tasks;
     
-    @Param({"RolezEager", "Rolez", "Java"})
+    @Param({"Rolez", "Java"})
     String impl;
     
     MonteCarloApp app;
